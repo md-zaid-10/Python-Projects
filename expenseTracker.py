edd5a92b656b6a9acc326796f7d451fe2046dc56
md-zaid-10 <mdzaid.de.123@gmail.com>
2026-01-26 13:12:53 +0100

    Created a file for Project 2

---
+++ expenseTracker.py
@@ -0,0 +1,9 @@
+# Create a console-based Expense Tracker program in python that allows the user to record daily expenses and view summaries like total spending. 
+
+# You are required to build  a simple personal dinanace management tool. Teh program should allow the user to:
+    # Add an expense with the details like date, category, description and amount
+    # View all recorded expenses in a clean format.
+    # Calculate total spending so far.
+    # Exit the program when the user chooses to.
+
+# No user-defined funcitons or file handling should be used.
