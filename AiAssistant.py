7dfa75dfa4ef0ee38c19a3e943feed5334c23f64
md-zaid-10 <mdzaid.de.123@gmail.com>
2026-01-27 17:55:56 +0100

    Created project file

---
+++ AiAssistant.py
@@ -0,0 +1,3 @@
+# OBJECTIVE : AI STUDY BUDDY (Rule-Based Chat Assistant in Python)
+# To create a conversational AI Assistant using Python's core logic - string matching, functions, dictionaries, and loops.
+
